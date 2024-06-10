d19974a6e893583adb018f64f55c34d42e15ac4b
dmitrij <dmitrijkrymskij6@gmail.com>
2024-06-10 22:50:11 +0400

    fix: animation

--- src/util/start-animation.ts
+++ src/util/start-animation.ts
@@ -1,6 +1,7 @@
 export async function startAnimation(time: number, race: HTMLElement) {
   race.style.transition = `transform ${time}ms linear`;
-  const finish = document.querySelector(".finish-img");
+  const raceField = race.parentNode?.parentNode;
+  const finish = raceField!.querySelector(".finish-img");
   const raceX = race.getBoundingClientRect();
   const finishX = finish!.getBoundingClientRect();
   const distance = Math.abs(finishX.right - raceX.left);
